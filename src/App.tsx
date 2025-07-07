ebedfd5287c9214982dcfcda87e4f868e40f7a26
JonnyWise25 <jwise25@cmc.edu>
2025-07-07 09:09:26 -0700

    updated api route

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { useEffect, useState } from 'react';
 function App() {
   const [status, setStatus] = useState('Loading...');
   useEffect(() => {
-    fetch('http://ec2-54-193-232-18.us-west-1.compute.amazonaws.com:3000/api/health')
+    fetch('https://api.ask-allie.com/api/health')
       .then(res => res.json())
       .then(data => setStatus(data.status))
       .catch(() => setStatus('Error connecting to backend'));
